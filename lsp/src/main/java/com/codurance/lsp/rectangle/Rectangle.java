bb0a964848e60be3625f1a613fd5a90a20912e35
Christian Ribeaud <christian.ribeaud@fhnw.ch>
2020-10-03 09:09:17 +0200

    Add rectangle spec. classes

--- lsp/src/main/java/com/codurance/lsp/rectangle/Rectangle.java
+++ lsp/src/main/java/com/codurance/lsp/rectangle/Rectangle.java
@@ -5,7 +5,16 @@ class Rectangle {
     private int width;
     private int height;
 
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
     public void setWidth(int width) {
+        assert width > -1;
         this.width = width;
     }
 
